54103b0fbe398cc27d1cbcebde6357d2fd94dab5
Bojan B <83359157+bonjadev@users.noreply.github.com>
2023-05-09 11:35:49 +0200

    First commit

---
+++ Demo2_Two_Numbers_comparation.cpp
@@ -0,0 +1,20 @@
+// Demo2_Two_Numbers_comparation.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 30;
+    int y = 30;
+    if
+        (x==y) {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+}
+
